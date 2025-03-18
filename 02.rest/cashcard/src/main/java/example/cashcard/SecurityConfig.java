c5411cf83f47fc65d15056ea167d13058f9d04a9
rsrisaart <rungthiwa.srisaart@gmail.com>
2025-03-19 03:49:33 +0700

    add testOnlyUser

--- 02.rest/cashcard/src/main/java/example/cashcard/SecurityConfig.java
+++ 02.rest/cashcard/src/main/java/example/cashcard/SecurityConfig.java
@@ -4,8 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -25,5 +29,16 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    UserDetailsService testOnlyUser(PasswordEncoder passwordEncoder) {
+        User.UserBuilder user = User.builder();
+        UserDetails sarah = user
+                .username("sarah1")
+                .password(passwordEncoder.encode("abc123"))
+                .roles()
+                .build();
+        return new InMemoryUserDetailsManager(sarah);
     }
 }
